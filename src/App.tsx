af54404e214409c82df99e18a060c48f55733c36
ansrlgur12 <ansrlgur12@naver.com>
2025-11-28 17:18:18 +0900

    라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './hooks/useAuth'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Signup } from './pages/Signup'
 
 function App() {
   // 앱 시작 시 인증 상태 복원 (refreshToken으로 accessToken + user 정보 가져오기)
   useAuth();
 
   return (
-    <>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
